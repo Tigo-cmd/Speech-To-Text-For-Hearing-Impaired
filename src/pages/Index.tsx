285671f6098d170af6754535c78c5b4b8ccb3a59
Tigo-cmd <ja16162040@gmail.com>
2025-06-20 16:46:55 +0100

    configures api endpoint for LAN devides

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -24,7 +24,7 @@ const Index = () => {
   const timerRef = useRef<NodeJS.Timeout | null>(null);
   
   const { toast } = useToast();
-  const apiEndpoint = "http://127.0.0.1:5000/transcribe"
+  const apiEndpoint = "http://192.168.214.48:5000/transcribe"
 
   useEffect(() => {
     return () => {
